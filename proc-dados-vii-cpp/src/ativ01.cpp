88d0a63e31dc921b754a6779ff26ed1c1a97a819
Fermyno Gutierrez <fermyno@gmail.com>
2022-11-15 14:08:07 +0000

    cpp file source

---
+++ proc-dados-vii-cpp/src/ativ01.cpp
@@ -0,0 +1,25 @@
+/*
+ * Curso: Tec. Informatica - Colegio Santa Familia
+ * Disciplina: Proc. Dados VII - C++
+ * Atividade: Programa que escreve o famoso "Ola mundo" na tela
+ *
+ * Instrucoes:
+ * Para criar o arquivo de objeto, execute o comando:
+ *   g++ -c file.cpp -o file.o
+ *   g++ file.o -o file
+ *
+ * Bibliografia complementar:
+ *   Introducao a Programacao em C++
+ *   Lucas S. Vieira
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ola Mundo!" << endl;
+    return 0;
+}
