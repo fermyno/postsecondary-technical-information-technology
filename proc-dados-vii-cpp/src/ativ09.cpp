4a5a4710b113e7e1f04c5958f1d3b8dce6f67017
Fermyno Gutierrez <fermyno@gmail.com>
2022-11-17 11:20:45 +0000

    cpp file source

---
+++ proc-dados-vii-cpp/src/ativ09.cpp
@@ -0,0 +1,33 @@
+/*
+ * Curso: Tec. Informatica - Colegio Santa Familia
+ * Disciplina: Proc. Dados VII - C++
+ * Atividade: Programa que abra um arquivo EXISTENTE e escreve uma mensagem secreta.
+ *            Atencao: o arquivo deve EXISTIR.
+ *
+ * Instrucoes:
+ * Para criar o arquivo de objeto, execute o comando:
+ *   g++ -c file.cpp -o file.o
+ *   g++ file.o -o file
+ *
+ * Bibliografia complementar:
+ *   Introducao a Programacao em C++
+ *   Lucas S. Vieira
+ *
+ */
+
+#include <fstream>
+
+int main()
+{
+    // escreve conteudo se arquivo existe
+    std::ofstream arq;
+    arq.open("arquivo.txt");
+    // Apenas escreva no arquivo se ele existe
+    if (arq.is_open())
+    {
+        arq << "Minha mensagem secreta!" << std::endl;
+        arq.close();
+    }
+
+
+}
